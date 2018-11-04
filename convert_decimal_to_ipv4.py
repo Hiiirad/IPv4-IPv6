52602e88f4d675eb86142c31dfd3496895e71761
Hirad Rasoolinejad <hirad.rad@gmail.com>
2018-11-04 10:52:30 +0330

    Change script to function.

--- convert_decimal_to_ipv4.py
+++ convert_decimal_to_ipv4.py
@@ -1,12 +1,14 @@
+def convert(number):
+    number = int(number)
+    first_octed = number // 256**3
+    # print(first_octed)
+    second_octed = (number % 256**3) // 256**2 
+    # print(second_octed)
+    third_octed = ((number % 256**3) % 256**2) // 256
+    # print(third_octed)
+    fourth_octed = ((number % 256**3) % 256**2) % 256
+    # print(fourth_octed)
+    return str(first_octed)+"."+str(second_octed)+"."+str(third_octed)+"."+str(fourth_octed)
 
-number = int(input("Please put your number here: "))
-# number = 176482051
-first_octed = number // 256**3 # khareje ghesmat
-# print(first_octed)
-second_octed = (number % 256**3) // 256**2 
-# print(second_octed)
-third_octed = ((number % 256**3) % 256**2) // 256
-# print(third_octed)
-fourth_octed = ((number % 256**3) % 256**2) % 256
-# print(fourth_octed)
-print(str(first_octed)+"."+str(second_octed)+"."+str(third_octed)+"."+str(fourth_octed))
+test = convert(176482051)
+print(test)
